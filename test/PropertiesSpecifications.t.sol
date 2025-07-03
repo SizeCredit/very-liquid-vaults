170137d9277277992d4a512bddb785200070d6ec
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-07-03 09:08:07 +0200

    Add rebalance property

--- test/PropertiesSpecifications.t.sol
+++ test/PropertiesSpecifications.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.23;
 
 abstract contract PropertiesSpecifications {
-    string constant PULL_ASSETS_01 = "PULL_ASSETS_01: transferAssets does not change balanceOf";
+    string constant REBALANCE_01 = "REBALANCE_01: rebalance does not change balanceOf";
+    string constant TRANSFER_ASSETS_01 = "TRANSFER_ASSETS_01: transferAssets does not change balanceOf";
     string constant STRATEGY_01 = "STRATEGY_01: Removing a strategy does not change balanceOf";
 }
