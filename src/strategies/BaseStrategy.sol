227c720adfb990add55122e7be6270e59ec3cc32
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-07-23 19:50:34 -0300

    Remove allowance override

--- src/strategies/BaseStrategy.sol
+++ src/strategies/BaseStrategy.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.23;
 import {BaseVault} from "@src/BaseVault.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {ERC20Upgradeable} from "@openzeppelin-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
 import {SIZE_VAULT_ROLE} from "@src/utils/Auth.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -13,24 +12,6 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 /// @author Size (https://size.credit/)
 /// @notice Abstract base contract for all strategies in the Size Meta Vault system
 abstract contract BaseStrategy is BaseVault {
-    /*//////////////////////////////////////////////////////////////
-                              ERC20 OVERRIDES
-    //////////////////////////////////////////////////////////////*/
-
-    /// @notice Returns the allowance of the spender for the owner
-    /// @dev Returns type(uint256).max if the spender has SIZE_VAULT_ROLE, since `rebalance` can be called to freely transfer assets between strategies
-    function allowance(address owner, address spender)
-        public
-        view
-        virtual
-        override(ERC20Upgradeable, IERC20)
-        returns (uint256)
-    {
-        return auth.hasRole(SIZE_VAULT_ROLE, spender) && owner == address(this)
-            ? type(uint256).max
-            : super.allowance(owner, spender);
-    }
-
     /*//////////////////////////////////////////////////////////////
                               ERC4626 OVERRIDES
     //////////////////////////////////////////////////////////////*/
